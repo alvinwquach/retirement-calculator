7aceb8903191a448da93e2f1ee2133333fd840f7
Alvin Quach <alvinwquach@gmail.com>
2023-05-16 15:02:43 -0700

    Add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDG7PBRzb9F3oYLklEI1Sb4pa0QSAY5hwc",
+  authDomain: "retirement-calculator-18a40.firebaseapp.com",
+  projectId: "retirement-calculator-18a40",
+  storageBucket: "retirement-calculator-18a40.appspot.com",
+  messagingSenderId: "546281613250",
+  appId: "1:546281613250:web:472cc48dac72c380ec585b",
+  measurementId: "G-6GYWYDS657",
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
